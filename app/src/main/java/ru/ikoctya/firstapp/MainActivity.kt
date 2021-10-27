7023e0455acedda30a09069de610268b7ffed194
ikoctya <ikoctya@gmail.com>
2021-10-27 13:34:46 +0400

    окей2=3

--- app/src/main/java/ru/ikoctya/firstapp/MainActivity.kt
+++ app/src/main/java/ru/ikoctya/firstapp/MainActivity.kt
@@ -1,16 +1,46 @@
 package ru.ikoctya.firstapp
 
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+
+private const val TAG = "MainActivity"
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+    override fun onStart(){
+       super.onStart()
+        Log.d(TAG, "onStart()")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d(TAG,"onResume()")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d(TAG,"onPause")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d(TAG,"onStop()")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(TAG,"onDestroy()")
+    }
+
+
+
 }
 
-fun alfabet{for(bukva in 'b'..'z' step 2){println(bukva)}}
 
 
 
---
+++ app/src/main/java/ru/ikoctya/firstapp/alphabet.kt
@@ -0,0 +1,5 @@
+package ru.ikoctya.firstapp
+
+// Вывод четных букв циклом
+fun alfabet(){for (bukva in 'b'..'z' step 2){println(bukva)}}
+
