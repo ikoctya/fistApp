0ec11acc895c5ff933ec8165515a5fe9f16855da
ikoctya <ikoctya@gmail.com>
2021-10-27 21:15:18 +0400

    туц05

--- app/src/main/java/ru/ikoctya/firstapp/MainActivity.kt
+++ app/src/main/java/ru/ikoctya/firstapp/MainActivity.kt
@@ -5,6 +5,16 @@ import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 
 private const val TAG = "MainActivity"
+private const val MY_OWN_LOG_TAG = "MyOwnLog"
+private const val VALUE = "Value"
+
+val pushkin  = listOf(
+    "Ты видел деву на скале \n В одежде белой над волнами",  //0
+    "Когда, бушуя в бурной мгле,\n Играло море с берегами",  //1
+    "Когда луч молний озарял \n Ее всечасно блеском алым",  //2
+    "И ветер бился и летал\n С ее летучим покрывалом?",  //3
+    "Прекрасно море в бурной мгле\n И небо в блесках без лазури;", //4
+    "Но верь мне: дева на скале \n  Прекрасней волн, небес и бури.") //5
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -14,27 +24,27 @@ class MainActivity : AppCompatActivity() {
 
     override fun onStart(){
        super.onStart()
-        Log.d(TAG, "1onStart()")
+        Log.d(MY_OWN_LOG_TAG, pushkin[0])
     }
 
     override fun onResume() {
         super.onResume()
-        Log.d(TAG,"2onResume()")
+        Log.d(MY_OWN_LOG_TAG,pushkin[1])
     }
 
     override fun onPause() {
         super.onPause()
-        Log.d(TAG,"3onPause")
+        Log.d(MY_OWN_LOG_TAG,pushkin[2])
     }
 
     override fun onStop() {
         super.onStop()
-        Log.d(TAG,"4onStop()")
+        Log.d(MY_OWN_LOG_TAG,pushkin[3])
     }
 
     override fun onDestroy() {
         super.onDestroy()
-        Log.d(TAG,"5onDestroy()")
+        Log.d(MY_OWN_LOG_TAG,pushkin[4])
     }
 
 }
